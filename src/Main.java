01acf3e6bbff4bea95b2c2a99635de9d93098fe3
Roman <rom_gn@hotmail.com>
2016-02-22 16:31:21 +0200

    version 0.1

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String args[]) {
 
         controls ctr = new controls("Calculator");
 
